2a810eea9cea11cc676a5a3284df5f610e6549e8
Mohammad Mawed <68829790+MohammadMawed@users.noreply.github.com>
2023-11-25 10:10:50 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); // Import the cors middleware
 const app = express();
 const port = 3000;
 
-// Specify the allowed origin in the cors options
-const corsOptions = {
-  origin: 'https://test-deploy-lac-seven.vercel.app/',
-};
-
-// Use the cors middleware with the specified options
-app.use(cors(corsOptions));
+// Use the cors middleware
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from your Node.js server!, Mohammmmmmad. This is easy!!!! ' });
